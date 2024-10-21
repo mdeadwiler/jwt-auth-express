4af570d8e5c6bc053d4f01a73d977ad7ef04c564
MDeadwiler <mdeadwiler@email.com>
2024-10-21 12:21:25 -0400

    did jwt-bcrypt-signInRouterPost-UserModel-UserController

--- model/user.js
+++ model/user.js
@@ -12,7 +12,11 @@ const userSchema = mongoose.Schema({
     required: true,
   },
 });
-
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.hashedPassword;
+    }
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
