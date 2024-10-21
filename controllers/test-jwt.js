62582752a9a206c9c1c540de95e1407805493b57
MDeadwiler <mdeadwiler@email.com>
2024-10-21 12:13:11 -0400

    first commit

---
+++ controllers/test-jwt.js
@@ -0,0 +1,29 @@
+// controllers/test-jwt.js
+
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+router.get('/sign-token', (req, res) => {
+ const user = {
+    _id: 1,
+    username: "testerton",
+    password: '1234',
+ };
+ const token = jwt.sign({ user }, process.env.JWT_SECRET);
+ //Send the token back to the client
+ res.json({ token });
+});
+
+router.post('/verify-token', (req, res) => {
+    try {
+    const token = req.headers.authorization.split(' ')[1];
+    // Now this will give a full verify response from using this bethod below
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.json({ decoded });
+} catch (error) {
+    res.status(401).json({ error: "Invalid token." });
+}
+});
+
+module.exports = router;
---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../model/user');
+
+const SALT_LENGTH = 12;
+
+
+router.post('/sign-up', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username });
+        if (userInDatabase) {
+            return res.status(400).json({ error: 'Username already taken.' });
+        }
+        const user = await User.create({
+            username: req.body.username,
+            hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
+        })
+        res.status(201).json({ user});
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+});
+
+module.exports = router;
